aa742bb401fa12a43286a9afc6f07764d24a0d4e
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-24 20:39:44 +0200

    mf

--- src/upload.py
+++ src/upload.py
@@ -2,12 +2,25 @@
 from tira.rest_api_client import Client
 from pathlib import Path
 from tqdm import tqdm
+import pandas as pd
 
 tira = Client()
-approach = 'ir-lab-sose-2024/tu-dresden-03/qe-gpt3.5-cot'
+approach = 'ir-lab-sose-2024/tu-dresden-03/qe-llama-cot'
 dataset_ids = ['ir-acl-anthology-20240504-training', 'ir-acl-anthology-topics-leipzig-20240423-test', 'ir-acl-anthology-topics-koeln-20240614-test', 'ir-acl-anthology-topics-augsburg-20240525_0-test']
 
 for dataset_id in tqdm(dataset_ids):
-    run_file = Path('generated') / 'chain-of-thoughts'/ 'gpt' / f'{dataset_id}.jsonl'
-    tira.upload_run(approach=approach, dataset_id=dataset_id, file_path=run_file);
+    path = Path('generated') / 'chain-of-thoughts'/ 'llama'
+    run_file = path / f'{dataset_id}.jsonl'
+
+    df = pd.read_json(run_file, lines=True)
+    df['qid'] = df['query-id'].astype(str)
+    df['query'] = df['response'].astype(str)
+    del df['query-id']
+    del df['response']
+    del df['query-text']
+    del df['metadata']
+    run_file = path / f'{dataset_id}.jsonl.gz'
+    df.to_json(run_file, lines=True, orient='records')
+
+   #tira.upload_run(approach=approach, dataset_id=dataset_id, file_path=run_file);
 
